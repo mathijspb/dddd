c33b6e9460482326c99cdba8a7087ffe3c92a8ff
Mathijs Peerboom <mathijspb@gmail.com>
2021-02-08 11:13:39 +0100

    :package: New build

--- rollup.lib.config.js
+++ rollup.lib.config.js
@@ -2,6 +2,7 @@ import html from 'rollup-plugin-html';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { string } from 'rollup-plugin-string';
+import { terser } from "rollup-plugin-terser";
 
 export default {
     input: 'src/dddd.js',
@@ -19,5 +20,6 @@ export default {
         }),
         nodeResolve(),
         commonjs(),
+        terser(),
     ],
 };
